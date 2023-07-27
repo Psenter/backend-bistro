ab0ee198eddc1dfe8a2e16cc8d5559da09c0e969
Parker S <111610335+Psenter@users.noreply.github.com>
2023-07-27 19:18:14 -0400

    changed some files

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import MenuItems, Category, Cuisine
+from django.http import JsonResponse
 
-# Create your views here.
+def get_menu(request):
+    menu_items = MenuItems.objects.select_related().all()
+    data = []
+
+    for item in menu_items:
+        data.append({
+            'title': item.title,
+            'description': item.description,
+            'price': item.price,
+            'spicy_level': item.spice_level,
+            'category': item.category.type,
+            'cuisine': item.cuisine.type
+        })
+
+    return JsonResponse(data, safe=False)
